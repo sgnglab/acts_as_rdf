59bbf3c6d6bea7d2fa8ca302fbaddc77adaf4054
Naoki Kawamukai <kna@slis.tsukuba.ac.jp>
2011-02-03 23:19:15 +0900

    モジュールの階層関係を修正

--- lib/acts_as_rdf/resource/active_model.rb
+++ lib/acts_as_rdf/resource/active_model.rb
@@ -1,23 +1,25 @@
 module ActsAsRDF
-  module ActiveModel
-    # このクラスの識別子を返す
-    # 識別子は、URIを16進文字列で表現した文字列である
-    #
-    # @return [String]
-    def id
-      encode_uri
-    end
-
-    private
-    
-    # 永続するデータかどうかの確認
-    # このクラスのものはRDF::Repositoryに保存されるはずなので
-    # trueを返す
-    # 
-    # @return [true]
-    # @see http://api.rubyonrails.org/classes/ActiveModel/Conversion.html
-    def persisted?
-      true
+  module Resource
+    module ActiveModel
+      # このクラスの識別子を返す
+      # 識別子は、URIを16進文字列で表現した文字列である
+      #
+      # @return [String]
+      def id
+        encode_uri
+      end
+      
+      private
+      
+      # 永続するデータかどうかの確認
+      # このクラスのものはRDF::Repositoryに保存されるはずなので
+      # trueを返す
+      # 
+      # @return [true]
+      # @see http://api.rubyonrails.org/classes/ActiveModel/Conversion.html
+      def persisted?
+        true
+      end
     end
   end
 end
